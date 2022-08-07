05f531f3ec0ac40a41e0ffdaad2c39abb5dcc817
cochis <ing.oarrs@gmail.com>
2022-08-06 22:02:31 -0500

    add white list https2

--- index.js
+++ index.js
@@ -62,29 +62,29 @@ app.use('/api/messages', require('./routes/messages'))
 app.get('*', function (req, res, next) {
   res.sendFile(path.resolve('client/index.html'))
 })
-// app.listen(process.env.PORT, () => {
-//   console.log(
-//     '__________________________________________________________________________________________________',
-//   )
-//   console.log(
-//     '__________________________________________________________________________________________________',
-//   )
-//   console.log('Servidor corriendo en puerto ' + process.env.PORT)
-// })
-https
-  .createServer(
-    {
-      cert: fs.readFileSync('/etc/letsencrypt/live/mylibam.com/fullchain.pem'),
-      key: fs.readFileSync('/etc/letsencrypt/live/mylibam.com/privkey.pem'),
-    },
-    app,
+app.listen(process.env.PORT, () => {
+  console.log(
+    '__________________________________________________________________________________________________',
   )
-  .listen(process.env.PORT, () => {
-    console.log(
-      '__________________________________________________________________________________________________',
-    )
-    console.log(
-      '__________________________________________________________________________________________________',
-    )
-    console.log('Servidor corriendo en puerto ' + process.env.PORT)
-  })
+  console.log(
+    '__________________________________________________________________________________________________',
+  )
+  console.log('Servidor corriendo en puerto ' + process.env.PORT)
+})
+// https
+//   .createServer(
+//     {
+//       cert: fs.readFileSync('/etc/letsencrypt/live/mylibam.com/fullchain.pem'),
+//       key: fs.readFileSync('/etc/letsencrypt/live/mylibam.com/privkey.pem'),
+//     },
+//     app,
+//   )
+//   .listen(process.env.PORT, () => {
+//     console.log(
+//       '__________________________________________________________________________________________________',
+//     )
+//     console.log(
+//       '__________________________________________________________________________________________________',
+//     )
+//     console.log('Servidor corriendo en puerto ' + process.env.PORT)
+//   })
