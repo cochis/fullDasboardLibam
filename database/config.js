469baa1cce2a391e47c7f84f90bd457363905c3d
cochis <ing.oarrs@gmail.com>
2022-08-23 19:57:18 -0500

    mongo

--- database/config.js
+++ database/config.js
@@ -3,6 +3,7 @@ dbConnection().catch((err) => console.log(err))
 
 async function dbConnection() {
   try {
+    console.log('process.env.DB_CNNLocal', process.env.DB_CNNLocal)
     await mongoose.connect(process.env.DB_CNNLocal)
     // await mongoose.connect(process.env.DB_CNN)
 
