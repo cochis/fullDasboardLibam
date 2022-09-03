255fd86424564b7ebe3373111a2ab74abc3ece7d
cochis <ing.oarrs@gmail.com>
2022-09-03 15:06:28 -0500

    Limpieza 3 septoembre

--- controllers/pagosCiclos.js
+++ controllers/pagosCiclos.js
@@ -330,7 +330,6 @@ const getPagosCiclosById = async (req, res) => {
 }
 const borrarCiclosById = async (req, res) => {
   const id = req.params.id
-  console.log('id', id)
   try {
     const cicloDB = await PagosCiclo.findById(id)
     if (!cicloDB) {
